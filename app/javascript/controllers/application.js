a2c001f32cb1da4a7671b0694a5d8baf5b183ca5
Olegs Osipovs <olegs.osipovs@gmail.com>
2025-03-14 14:46:12 +0200

    Devise error helper upadted

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,8 @@
 import { Application } from "@hotwired/stimulus"
 import Dropdown from "stimulus-dropdown"
+import "@hotwired/turbo-rails"
+import "controllers"
+import "lib/flash"
 
 const application = Application.start()
 application.register("dropdown", Dropdown)
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme') 
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+  safelist: [
+    "bg-orange-100", "border-orange-400", "text-orange-700",
+    "bg-red-100", "border-red-400", "text-red-700",
+    "bg-yellow-100", "border-yellow-400", "text-yellow-700",
+    "bg-greem-100", "border-greem-400", "text-green-700",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    content: [
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+      './app/views/**/*.{erb,haml,html,slim}'
+  ], theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter var', ...defaultTheme.fontFamily.sans],
   },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
-  ]
-}
+  }, },
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/typography')
+  ] }
